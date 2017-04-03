04f66bbcdc32e30fd3db47348b7a673961177476
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-03 16:59:07 -0600

    create a sign in view component

---
+++ src/app/sign-in-view-component/sing-in-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sign-in-view',
+  templateUrl: 'app/sign-in-view-component/sign-in-view.component'
+})
+
+export class SignInViewComponent {}
