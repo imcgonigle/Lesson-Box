9e208097692faeba0bbc6385918cf2f6bb7ef671
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-02 22:09:41 -0600

    import the dashboardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard-component/dashboard.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent,
+    DashboardComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
