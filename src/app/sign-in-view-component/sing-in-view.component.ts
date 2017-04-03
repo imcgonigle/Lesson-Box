f99a0424aaa8c7cd261e1974e5c2f7e83806999d
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-03 17:12:15 -0600

    create a dummy sing in method

--- src/app/sign-in-view-component/sing-in-view.component.ts
+++ src/app/sign-in-view-component/sing-in-view.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sign-in-view',
   templateUrl: 'app/sign-in-view-component/sign-in-view.component.html'
 })
 
-export class SignInViewComponent {}
+export class SignInViewComponent {
+  constructor(private router: Router){}
+
+  signIn(): void{
+    this.router.navigate(['/dashboard'])
+  }
+}
