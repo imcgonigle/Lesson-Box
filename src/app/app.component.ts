be73ab92908c93c558afa2ff3ffcb53d079a66c3
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-02 22:12:24 -0600

    use dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'lesson-box-app',
-  template: `<h1>{{name}}</h1>`,
+  template: `
+    <h1>{{name}}</h1>
+    <dashboard></dashboard>
+  `,
 })
 export class AppComponent  { name = 'Lesson Box'; }
