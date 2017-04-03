f875f589224e5ce035c75ee97279ca35881676e8
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-03 16:22:49 -0600

    import appRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {AppRoutingModule } from './app-routing-module/app-routing.module';
+
 import { AppComponent }  from './app.component';
 import { HeaderComponent } from './header-component/header.component';
 import { DashboardComponent } from './dashboard-component/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule,
+    AppRoutingModule
+  ],
   declarations: [ 
     AppComponent,
     HeaderComponent,
