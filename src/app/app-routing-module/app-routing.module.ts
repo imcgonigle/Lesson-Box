d4e8d8a3895067e42661fb3600721a3bff38b452
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-03 17:00:53 -0600

    add sign in view to route

--- src/app/app-routing-module/app-routing.module.ts
+++ src/app/app-routing-module/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from '../dashboard-component/dashboard.component';
+import { SignInViewComponent} from '../sign-in-view-component/sing-in-view.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/dashboard', pathMatch:'full'},
+  {path: '', redirectTo:'/signin', pathMatch:'full'},
+  {path: 'signin', component: SignInViewComponent},
   {path: 'dashboard', component: DashboardComponent}
 ];
 
