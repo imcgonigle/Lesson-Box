ae70a528cc860458096daa0a00e0ad06c48e5b79
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-03 16:11:02 -0600

    import HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HeaderComponent } from './header-component/header.component';
 import { DashboardComponent } from './dashboard-component/dashboard.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ 
     AppComponent,
+    HeaderComponent,
     DashboardComponent
   ],
   bootstrap:    [ AppComponent ]
