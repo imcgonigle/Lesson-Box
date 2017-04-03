26887ee7872159eaa90f56d197b2aa14a10eabf9
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-03 17:25:10 -0600

    import styles for header

--- src/app/header-component/header.component.ts
+++ src/app/header-component/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: 'app/header-component/header.component.html'
+  templateUrl: 'app/header-component/header.component.html',
+  styleUrls: ['app/header-component/header.component.css']
 })
 
 export class HeaderComponent {}
