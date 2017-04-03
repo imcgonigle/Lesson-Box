ad662e0668edd5ae2451f997c61a440f2dd88afd
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-03 17:02:59 -0600

    add file extension to end of url

--- src/app/sign-in-view-component/sing-in-view.component.ts
+++ src/app/sign-in-view-component/sing-in-view.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sign-in-view',
-  templateUrl: 'app/sign-in-view-component/sign-in-view.component'
+  templateUrl: 'app/sign-in-view-component/sign-in-view.component.html'
 })
 
 export class SignInViewComponent {}
